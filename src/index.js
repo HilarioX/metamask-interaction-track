61fea35106a3a01163d8b5aeeb17156f6947ba17
HilarioX <juninho.gomes1234@gmail.com>
2023-07-20 23:17:51 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const { MetaMaskSDK } = require('@metamask/sdk');
+const ethers = require('ethers')
+
+const MMSDK = new MetaMaskSDK({
+    dappMetadata: {
+        name: 'Test name',
+        url: 'yoursite.com',
+    },
+    showQRCode: true,
+    shouldShimWeb3: true
+});
+
+const ethereum = MMSDK.getProvider();
+
+async function connect() {
+    await ethereum.request({ method: 'eth_requestAccounts', params: [] });
+    console.log('connected')
+
+    ethereum.on('chainChanged', (chainId) => {
+        console.log('changed to:', chainId)
+    })
+
+}
+
+connect()
